3c45a404423c9fbf3d520b0b0853e82b3e5eda95
Victoria Truong <victoriataitruong@gmail.com>
2021-05-19 09:51:37 -0700

    added new commands

--- projects/demo-app-3/demo.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-r = requests.get('http://www.example.com')
-soup = BeautifulSoup(r.text, features="html.parser")
-print(soup.text)
---
+++ script.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+
+print(np.std([1, 17, 32, 4]))
