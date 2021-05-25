a40cc31b3d467e2c8f70deabec533bc0da50f38f
Victoria Truong <victoriataitruong@gmail.com>
2021-05-25 13:02:09 -0700

    scaped all data for one book and save to a file

--- project2_deliverables/single_product_scrape.py
+++ project2_deliverables/single_product_scrape.py
@@ -1,42 +1,32 @@
-"""
-product_page_url*
-universal_ product_code (upc)* 
-title*
-price_including_tax*
-price_excluding_tax*
-number_available
-product_description
-category*
-review_rating
-image_url*
-"""
 # requests
 import requests
 import csv 
+
 # beautifulSoup
 from bs4 import BeautifulSoup
+
 # url to scrape
-url = "http://books.toscrape.com/catalogue/walt-disneys-alice-in-wonderland_777/index.html"
-page = requests.get(url)
+book_url = "http://books.toscrape.com/catalogue/walt-disneys-alice-in-wonderland_777/index.html"
+page = requests.get(book_url)
+
 # turn HTML to BeautifulSoup object
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# create a list to hold scaped data
-book_data = []
-# adding product_page_url
-book_data.append(url)
-# adding universal_ product_code
-universal_product_code = soup.tr
-book_data.append(universal_product_code)
-# adding page title to list 
-title = soup.title
-book_data.append(title)
-# adding price_including_tax
-price_including_tax = soup.tr.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
-book_data.append(price_including_tax)
-# adding price_excluding_tax
-price_excluding_tax = soup.tr.next_sibling.next_sibling.next_sibling.next_sibling
-book_data.append(price_excluding_tax)
-# printing out list
-print(*book_data, sep='\n')
+# open csv file and write headings
+f = csv.writer(open('book_data.csv', 'w'))
+f.writerow(["url", 'universal_product_code', 'title', 'price_including_tax','price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url'])
 
+# scape data 
+url = book_url,
+universal_product_code = soup.tr, 
+title = soup.title,
+price_including_tax = soup.tr.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling,
+price_excluding_tax = soup.tr.next_sibling.next_sibling.next_sibling.next_sibling,
+number_available = soup.tr.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling,
+product_description = soup.find('meta', attrs={'name': 'description'})
+category = soup.li.next_sibling.next_sibling.next_sibling.next_sibling,
+review_rating = soup.p.next_sibling.next_sibling.next_sibling.next_sibling,
+image_url = soup.img
+
+# write scaped data to csv file
+f.writerow([url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
