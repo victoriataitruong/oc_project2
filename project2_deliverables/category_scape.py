72f6859a02a879a7dda7075897216d0e5f33f245
Victoria Truong <victoriataitruong@gmail.com>
2021-05-27 08:56:42 -0700

    can scape data from multiple pages

--- project2_deliverables/category_scape.py
+++ project2_deliverables/category_scape.py
@@ -1,15 +1,15 @@
-# import libs
 import requests
 import csv 
 from bs4 import BeautifulSoup
 
 additional_pages = True
 category_url = "http://books.toscrape.com/catalogue/category/books/default_15/index.html"
-page = requests.get(category_url)
-soup = BeautifulSoup(page.content, 'html.parser')
+
 category_url_additional_page_count = 1
 ending_urls = []
 while(additional_pages): 
+    page = requests.get(category_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
     ending_urls.append(soup.find_all(["h3"]))
     category_url_additional_page_count+= 1
     category_url_additional_page_count_str = str(category_url_additional_page_count)
