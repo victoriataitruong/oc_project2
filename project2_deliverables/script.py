bf46108e6e1781d346388f8a780f32480bfd3b78
Victoria Truong <victoriataitruong@gmail.com>
2021-05-29 12:48:35 -0700

    scaped data from full category

--- project2_deliverables/script.py
+++ project2_deliverables/script.py
@@ -1,6 +1,3 @@
-txt = "apple#banana#cherry#orange"
-
-# setting the maxsplit parameter to 1, will return a list with 2 elements!
-x = txt.split("#", 1)
-
-print(x)
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
