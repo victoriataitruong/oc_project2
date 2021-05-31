076ead41e209584b5d52f074608bb468fa955e45
Victoria Truong <victoriataitruong@gmail.com>
2021-05-31 11:40:46 -0700

    grabbed all book urls

--- project2_deliverables/data_all_categories.py
+++ project2_deliverables/data_all_categories.py
@@ -1,8 +1,9 @@
 # import libs
 import requests
 import csv 
-import re
 from bs4 import BeautifulSoup
+import re
+
 
 # url to scrape from
 book_url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
@@ -17,12 +18,23 @@ book_categories = []
 # looping through and getting all links
 for link in soup.find_all('a'):
     book_categories.append(link.get('href', {"class":"side_categories"}))
-# 
+
+# list to hold all urls
 new_book_categories = []
-# index 3 to 52
+# grabbing only category urls
 for i in range(3, 53):
     new_book_categories.append(book_categories[i])
 
+# add url prefixes. new_book_categories now has all book urls for bookstoscape
 new_book_categories = [w.replace('../', 'http://books.toscrape.com/catalogue/category/') for w in new_book_categories]
 
-print (new_book_categories)
+# visit each category page and extract all book urls
+book_urls = []
+for i in new_book_categories: 
+    page = requests.get(i)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup_string = str(soup.find_all("h3"))
+    book_urls.append(soup_string)
+book_urls = [w.replace('../../../', 'http://books.toscrape.com/catalogue/') for w in book_urls]  
+book_urls = [re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', str(book_urls))]
+print(book_urls)
